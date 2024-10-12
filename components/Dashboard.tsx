ab9f809237fef6b9abb9a4d3fdbe233065dd2c61
Camilo Hernandez <hrcamilo11@gmail.com>
2024-10-12 15:15:11 -0500

    Initial Commit: Backend

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -8,7 +8,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import {
-  CalendarDays,
   Package,
   Scissors,
   Search,
